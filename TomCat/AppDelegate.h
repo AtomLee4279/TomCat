a82197791b03b89be6f6331a5a25db45af6aecb9
李一贤 <liyixian@atomlee-imac.local>
2018-03-12 16:08:58 +0800

    Initial Commit

---
+++ TomCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TomCat
+//
+//  Created by 李一贤 on 2018/3/12.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

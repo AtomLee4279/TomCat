50b4fb517b99c02527078d54a892bdc95598ed28
李一贤 <liyixian@atomlee-imac.local>
2018-03-12 18:32:37 +0800

    添加一些图片资源

--- TomCat/ViewController.m
+++ TomCat/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)drink;
+@property (weak, nonatomic) IBOutlet UIImageView *Tom;
 
 @end
 
@@ -26,4 +28,16 @@
 }
 
 
+- (IBAction)drink
+{
+    //获取图片
+    for(int i = 0;i<81;i++)
+    {
+        //%02d，表示这个数字有两位，不足两位用占位符0补足;
+        //png格式的图片可以不加格式后缀，jpg最好加上
+        NSString*name = [NSString stringWithFormat:@"drink_%02d.jpg",i];
+        UIImage *headImage = [UIImage imageNamed:name];
+        NSLog(@"%@",name);
+    }
+}
 @end

72bf9c1d19db1a72fa12386272647df02e006de8
李一贤 <liyixian@atomlee-imac.local>
2018-03-13 15:54:51 +0800

    添加播放音效的具体实现

--- TomCat/ViewController.m
+++ TomCat/ViewController.m
@@ -7,11 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
 - (IBAction)doAction:(UIButton*)btn;
 @property (weak, nonatomic) IBOutlet UIImageView *Tom;
-
+@property(strong,nonatomic) AVAudioPlayer* myPlayer;
 @end
 
 @implementation ViewController
@@ -33,9 +34,12 @@
     switch (btn.tag) {
         case 1:
             [self runAnimationWithName:@"drink" andWithCount:81];
+//            [NSThread cancelPreviousPerformRequestsWithTarget:self selector:@selector(playSound) object:self];
             break;
         case 2:
             [self runAnimationWithName:@"knockout" andWithCount:81];
+            //让播放音效延迟1.0秒后再执行
+            [self performSelector:@selector(playSound) withObject:nil afterDelay:1.0];
             break;
         default:
             break;
@@ -56,7 +60,6 @@
         //png格式的图片可以不加格式后缀，jpg最好加上
         NSString*name = [NSString stringWithFormat:@"%@_%02d.jpg",imageName,i];
         UIImage *headImage = [UIImage imageNamed:name];
-        NSLog(@"%@",name);
         [images addObject:headImage];
     }
 //2.告诉Tom要播放哪些图片
@@ -67,5 +70,18 @@
     self.Tom.animationRepeatCount = 1;
 //5.播放动画
     [self.Tom startAnimating];
+}
+
+-(void)playSound
+{
+//1.获取音效资源的路径
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"Tom" ofType:@"wav"];
+//2.把路径path转化成url
+    NSURL *url = [NSURL fileURLWithPath:path];
+//3.建立一个播放器
+    NSError *error = nil;
+    AVAudioPlayer *player =[[AVAudioPlayer alloc]initWithContentsOfURL:url error:&error];//注意，这里的player是个局部变量，播放音效过程中该函数已经跑完。此时该变量会被销毁；因 此若使用局部变量调用播放，将出现没有听到预期音效的bug。解决方法：将它指向一个strong的该类成员变量
+     _myPlayer = player;
+    [_myPlayer play];
 }
 @end

d4bca6fecf4492c32bc7eef2a200ab48c5faacd6
李一贤 <liyixian@atomlee-imac.local>
2018-03-13 12:33:55 +0800

    序列帧播放动画的具体实现及代码结构优化

--- TomCat/ViewController.m
+++ TomCat/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-- (IBAction)drink;
+- (IBAction)doAction:(UIButton*)btn;
 @property (weak, nonatomic) IBOutlet UIImageView *Tom;
 
 @end
@@ -28,16 +28,44 @@
 }
 
 
-- (IBAction)drink
+- (IBAction)doAction:(UIButton*)btn
 {
-    //获取图片
-    for(int i = 0;i<81;i++)
+    switch (btn.tag) {
+        case 1:
+            [self runAnimationWithName:@"drink" andWithCount:81];
+            break;
+        case 2:
+            [self runAnimationWithName:@"knockout" andWithCount:81];
+            break;
+        default:
+            break;
+    }
+    
+}
+
+- (void)runAnimationWithName:(NSString*)imageName andWithCount:(int)count
+{
+//0.判断当前是否正在执行动画,如果是，则直接返回
+    if(self.Tom.isAnimating)
+        return;
+//1.获取图片
+    NSMutableArray *images = [NSMutableArray array];
+    for(int i = 0;i<count;i++)
     {
         //%02d，表示这个数字有两位，不足两位用占位符0补足;
         //png格式的图片可以不加格式后缀，jpg最好加上
-        NSString*name = [NSString stringWithFormat:@"drink_%02d.jpg",i];
+        NSString*name = [NSString stringWithFormat:@"%@_%02d.jpg",imageName,i];
         UIImage *headImage = [UIImage imageNamed:name];
         NSLog(@"%@",name);
+        [images addObject:headImage];
     }
+//2.告诉Tom要播放哪些图片
+    self.Tom.animationImages = images;
+//3.根据图片张数设置动画播放时间
+    self.Tom.animationDuration = images.count*0.05 ;
+//4.设置动画重复播放次数
+    self.Tom.animationRepeatCount = 1;
+//5.播放动画
+    [self.Tom startAnimating];
 }
 @end
